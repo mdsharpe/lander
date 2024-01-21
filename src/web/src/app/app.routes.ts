8434f001b6b692dfd14e9aa997b3e52bd269ad11
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2024-01-21 13:34:04 +0000

    hungry horse coding

--- src/web/src/app/app.routes.ts
+++ src/web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: 'play',
+        loadChildren: () =>
+            import('./features/game/game.module').then((m) => m.GameModule),
+    },
+];
